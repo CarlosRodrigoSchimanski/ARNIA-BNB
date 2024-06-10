beb7d532c57e62b1202195d13d8d488219b141d3
CarlosRodrigoSchimanski <76493107+CarlosRodrigoSchimanski@users.noreply.github.com>
2024-06-10 02:41:50 -0300

    new

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import router from './routes'
 
 const app = express()
 app.use(express.json())
-app.use(express.static("uploads/"))
+app.use(express.static("src/uploads/"))
 app.use("/",consoleLog)
 app.use("/",router)
 
